3332463a0c2ea0f6ae0621e82413090a21d4c376
ngonzalez <nestor.ge222@gmail.com>
2022-04-15 17:48:39 -0600

    update del login

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,13 @@
 import React, { useState, useContext, createContext } from 'react';
 import Cookie from 'js-cookie';
 import axios from 'axios';
+import endPoints from '@services/api/';
 
 const AuthContext = createContext();
-export function ProviderAuth({ childern }) {
+
+export function ProviderAuth({ children }) {
   const auth = useProvideAuth();
-  return <AuthContext.Provider value={auth}>;{childern}</AuthContext.Provider>;
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 }
 
 export const useAuth = () => {
@@ -16,7 +18,14 @@ function useProvideAuth() {
   const [user, setUser] = useState(null);
 
   const signIn = async (email, password) => {
-    setUser('login');
+    const options = {
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
+    console.log(access_token);
   };
 
   return {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,15 @@
 import MainLayout from '@layout/MainLayout';
-import { ProviderAuth } from "@hooks/useAuth"
+import { ProviderAuth } from '@hooks/useAuth';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <ProviderAuth>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-
-    </ProviderAuth>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
     </>
   );
 }
