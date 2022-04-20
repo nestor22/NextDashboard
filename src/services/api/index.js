de82346f3621ae6dc756799c20ea1ca80e57b5cd
ngonzalez <nestor.ge222@gmail.com>
2022-04-19 21:53:05 -0600

    fix dahsboard images

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,8 +1,8 @@
 const API = process.env.NEXT_PUBLIC_API_URL;
-const VERSION = process.env.NEST_PUBLIC_API_VERSION;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 const endPoints = {
   products: {
-    getProducts:(limit, offset) =>`${API}/api/${VERSION}/products?limit${limit}&offset=${offset}`,
+    getProducts:(limit, offset) =>`${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
     postProducts: `${API}/api/${VERSION}/products/`,
     getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
     putProducts: (id) => `${API}/api/${VERSION}/products/${id}`,
